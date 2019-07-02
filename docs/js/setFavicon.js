af833701024692291d84c50fc8745e9cc188ef90
Buscedv <edvard1807@gmail.com>
2019-07-02 22:06:26 +0300

    New design for wiki

--- docs/js/setFavicon.js
+++ docs/js/setFavicon.js
@@ -1,10 +1,13 @@
-var api_url = "https://algoryalapi.firebaseio.com/Media/Images/Logos/web_based_logo.json"
-var Httpreq = new XMLHttpRequest(); // a new request
-Httpreq.open("GET",api_url, false);
-Httpreq.send(null);
+function setFavicon() {
+	var api_url = "https://algoryalapi.firebaseio.com/Media/Images/Logos/web_based_logo.json"
+	var Httpreq = new XMLHttpRequest(); // a new request
+	Httpreq.open("GET",api_url, false);
+	Httpreq.send(null);
 
-var json_obj = Httpreq.responseText;
-json_obj = json_obj.replace(/['"]+/g, '');
+	var json_obj = Httpreq.responseText;
+	json_obj = json_obj.replace(/['"]+/g, '');
 
-var favicon_link = document.getElementById('favicon');
-favicon_link.href = json_obj;
+	var favicon_link = document.getElementById('favicon');
+	favicon_link.href = json_obj;
+}
+export {setFavicon}
